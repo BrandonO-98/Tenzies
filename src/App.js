21064b396ec9c14fb113b21a49741347b77212bb
BrandonO-98 <brandonshew98@gmail.com>
2022-05-01 11:18:59 -0400

    Revert back to manually clicking ROLL so that the new version can be put on a different branch.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Die from "./components/Die.js";
 // import dieData from "./dieData.js";
-import { nanoid } from "nanoid";
+import {nanoid} from "nanoid";
 import Confetti from "react-confetti";
 
 function App() {
@@ -36,10 +36,9 @@ function App() {
       oldDice.map((die) => {
         return die.isStatic
           ? die
-          : { ...die, value: Math.ceil(Math.random() * 6) };
+          : {...die, value: Math.ceil(Math.random() * 6)};
       })
     );
-    setTimeout(roll, 1000);
   }
 
   function newGame() {
@@ -50,7 +49,7 @@ function App() {
   function hold(id) {
     setDice((oldDice) =>
       oldDice.map((die) => {
-        return die.id === id ? { ...die, isStatic: !die.isStatic } : die;
+        return die.id === id ? {...die, isStatic: !die.isStatic} : die;
       })
     );
   }
@@ -59,20 +58,20 @@ function App() {
     <Die key={die.id} obj={die} holdFunc={() => hold(die.id)} />
   ));
   return (
-    <div className="App">
+    <div className='App'>
       <main>
         {tenzies && <Confetti />}
-        <h1 className="title">Tenzies</h1>
-        <p className="rules">
+        <h1 className='title'>Tenzies</h1>
+        <p className='rules'>
           Click each die to freeze its value between rolls. Freeze all dice with
           the same value to win.
         </p>
-        <div className="dice-container"> {dieElements}</div>
+        <div className='dice-container'> {dieElements}</div>
         <button
           className={`btn ${tenzies ? `newGame` : `roll`}`}
           onClick={tenzies ? newGame : roll}
         >
-          {tenzies ? `New Game` : `Start`}
+          {tenzies ? `New Game` : `Roll`}
         </button>
       </main>
     </div>
